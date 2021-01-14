90a44781725f3e0c3c3119d34779d5df9d08432e
Kevin Meldau <me@kevinmeldau.com>
2021-01-14 14:47:25 -0500

    First API call

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-document.getElementById("dropdown").addEventListener("click", function(e) {
-  e.stopPropagation();
-});
+function getImageOfTheDay() {
+  fetch(
+    "https://api.nasa.gov/planetary/apod?api_key=TMwH0mQYVUz8HClsf21Q31IHtjcL90svDqRnwiKy"
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.url);
+      const imageElement = document.getElementById("photo-of-the-day");
+      imageElement.src = data.url;
+    })
+    .catch((error) => {
+      console.log("ERROR: ", error);
+    });
+}
+
+getImageOfTheDay();
+
+
+// Setup an event listener on the left and right buttons
+// Add click event to left and right buttons
+// Checkout CSS keyframes
