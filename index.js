9f416e44ec6a92c70094aa6f9fc299acb92001e4
Kevin Meldau <me@kevinmeldau.com>
2021-01-14 15:04:00 -0500

    Did some stuff

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-function getImageOfTheDay() {
+// function getImageOfTheDay() {
+//   fetch(
+//     "https://api.nasa.gov/planetary/apod?api_key=TMwH0mQYVUz8HClsf21Q31IHtjcL90svDqRnwiKy"
+//   )
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log(data.url);
+//       const imageElement = document.getElementById("photo-of-the-day");
+//       imageElement.src = data.url;
+//     })
+//     .catch((error) => {
+//       console.log("ERROR: ", error);
+//     });
+// }
+// getImageOfTheDay();
+
+// My attempt at hooking up the Mars images API
+
+function getMarsImages() {
   fetch(
     "https://api.nasa.gov/planetary/apod?api_key=TMwH0mQYVUz8HClsf21Q31IHtjcL90svDqRnwiKy"
   )
@@ -14,10 +34,10 @@ function getImageOfTheDay() {
       console.log("ERROR: ", error);
     });
 }
-
-getImageOfTheDay();
-
+getMarsImages();
 
 // Setup an event listener on the left and right buttons
 // Add click event to left and right buttons
 // Checkout CSS keyframes
+
+// <!-- API Key - https://api.nasa.gov/planetary/apod?api_key=TMwH0mQYVUz8HClsf21Q31IHtjcL90svDqRnwiKy -->
